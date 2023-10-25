2af15bb22e6796fcfd1b5f7fbe589762b04d3c05
appsterlight <appsterlight@gmail.com>
2023-10-25 17:35:20 +0300

    Refactor and optimize doFilterInternal() in JwtRequestFilter class

--- src/main/java/co/inventorsoft/academy/spring/restfull/filter/JwtRequestFilter.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/filter/JwtRequestFilter.java
@@ -2,8 +2,9 @@ package co.inventorsoft.academy.spring.restfull.filter;
 
 import co.inventorsoft.academy.spring.restfull.service.JwtUserDetailsService;
 import co.inventorsoft.academy.spring.restfull.util.JwtTokenUtil;
-import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter  {
@@ -28,42 +30,43 @@ public class JwtRequestFilter extends OncePerRequestFilter  {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String requestTokenHeader = request.getHeader("Authorization");
-
-        String username = null;
-        String jwtToken = null;
-        // JWT Token is passed in the form "Bearer <token>".
-        //  Thus, let's remove Bearer word and get the Token
-        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-            jwtToken = requestTokenHeader.substring(7);
-            try {
-                username = jwtTokenUtil.getUsernameFromToken(jwtToken);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Failed to get token");
-            } catch (ExpiredJwtException e) {
-                System.out.println("JWT Token is expired");
-            }
-        } else {
-            System.out.println("JWT Token does not begin with Bearer word");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // Get authorization header and validate
+        final String tokenHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
         }
 
-        //Validate the token
+        // Get jwt token and validate
+        final String token = tokenHeader.substring(7);
+        if (!jwtTokenUtil.validate(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get user identity and set it on the spring security context
+        String username = jwtTokenUtil.getUsernameFromToken(token);
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
-            UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
-
-            // on valid token configure Spring Security to authentication
-            if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken
-                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            UserDetails userDetails;
+            try {
+                userDetails = jwtUserDetailsService.loadUserByUsername(username);
+            } catch (UsernameNotFoundException e) {
+                userDetails = null;
             }
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userDetails, null,
+                    userDetails == null ? List.of() : userDetails.getAuthorities()
+            );
+
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         filterChain.doFilter(request, response);
     }
 }
