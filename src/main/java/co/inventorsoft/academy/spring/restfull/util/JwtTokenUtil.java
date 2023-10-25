83d27f938da6e7dfc8c2a3d0d14cfa6fdcaa7857
appsterlight <appsterlight@gmail.com>
2023-10-25 17:25:42 +0300

    Introduce validate method in JwtTokenUtil with token parameter and enhance exception handling for getAllClaimsFromToken() with logging

--- src/main/java/co/inventorsoft/academy/spring/restfull/util/JwtTokenUtil.java
+++ src/main/java/co/inventorsoft/academy/spring/restfull/util/JwtTokenUtil.java
@@ -1,13 +1,20 @@
 package co.inventorsoft.academy.spring.restfull.util;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Claims;
 
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,8 +22,10 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Component
+@Slf4j
 public class JwtTokenUtil implements Serializable {
-    private static final long serialVersionUID = -2550185165626007488L;
+    @Serial
+    private static final long serialVersionUID = 1L;
     public static final long JWT_TOKEN_VALIDITY = 2 * 60 * 60; //will expire in 2 hours
 
     @Value("${jwt.secret}")
@@ -36,22 +45,51 @@ public class JwtTokenUtil implements Serializable {
         return claimsResolver.apply(claims);
     }
 
-
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
 
         return doGenerateToken(claims, userDetails.getUsername());
     }
 
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
 
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
+    public boolean validate(String token) {
+        try {
+            Claims claims = getAllClaimsFromToken(token);
+
+            if (isTokenExpired(token)) {
+                return false;
+            }
+
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        try {
+            return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            log.error(String.format("JWT Token has expired! %s", token));
+            throw e;
+        } catch (UnsupportedJwtException e) {
+            log.error(String.format("JWT Token is unsupported! %s", token));
+            throw e;
+        } catch (MalformedJwtException e) {
+            log.error(String.format("JWT Token is malformed! %s", token));
+            throw e;
+        } catch (SignatureException e) {
+            log.error(String.format("JWT Token signature error! %s", token));
+            throw e;
+        } catch (IllegalArgumentException e) {
+            log.error(String.format("Failed to parse token! %s", token));
+            throw e;
+        }
     }
 
     private Boolean isTokenExpired(String token) {
